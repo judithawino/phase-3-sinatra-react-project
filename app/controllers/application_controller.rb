2ebb0f4e5ef2c0a8d245344a622702c20ba90e50
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-09-13 12:44:54 +0300

    Added route pathe to create and delete song

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/artists" do
-    artists = Artist.all.order(:name).limit(15)
+    artists = Artist.all
     artists.to_json(only: [:id, :name, :birth_date, :hometown, :country], include: {     
         albums: { only: [:id, :album_name] }
       } 
@@ -23,18 +23,45 @@ class ApplicationController < Sinatra::Base
       artist.to_json(include: {albums: {only: [:id, :album_name]}})
   end
 
+  post '/artists' do
+    artist = Artist.create(
+      name: params[:name],
+      birth_date: params[:birth_date],
+      hometown: params[:hometown],
+      country: params[:country]      
+    )
+    artist.to_json
+  end
+
   get "/albums" do
       albums = Album.all.order(:album_name).limit(20)
       albums.to_json(except: [:artist_id], include: {artist: {only: [:id, :name]}})
   end
 
+  post "/albums" do
+    albums = Album.create(
+      album_name: params[:album_name],
+      artist_id: params[:artist_id],
+      launched_at: params[:launched_at]   
+   
+    )
+    albums.to_json
+  end
+
   get '/genres' do
-    genre = Genre.all.order(:genre_name).limit(10)
+    genre = Genre.all
+    genre.to_json
+  end
+  post '/genres' do
+    genre = Genre.create(
+      genre_name: params[:genre_name],
+      created_at: params[:genre_name]
+    )
     genre.to_json
   end
 
   get '/songs' do
-  song = Song.all.order(:title).limit(100)
+  song = Song.all
 
   # include associated reviews in the JSON response  
   song.to_json(only: [:id, :title, :has_lyrics ], include: { artist: {only: [:id, :name], include: {genres: {only: [:id, :genre_name] } }} })
@@ -54,8 +81,8 @@ class ApplicationController < Sinatra::Base
   patch '/songs/:id' do
     song = Song.find(params[:id])
     song.update(
-      title: params[:title],
-      has_lyrics: params[:has_lyrics]
+      title: params[:title]
+      
     )
     song.to_json
   end
